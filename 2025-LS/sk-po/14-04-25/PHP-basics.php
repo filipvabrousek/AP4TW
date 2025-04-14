1ec0f3178700f5ca0833d9503521196173891b70
Filip Vabrousek <filipvabrousek1@gmail.com>
2025-04-14 17:31:42 +0200

    Update PHP-basics.php

--- 2025-LS/sk-po/14-04-25/PHP-basics.php
+++ 2025-LS/sk-po/14-04-25/PHP-basics.php
@@ -4,7 +4,7 @@ echo "Hello";
 $name = "Eda";
 echo $name;
 
-
+echo "\n";
 $age = 21;
 
 if ($age > 21){
@@ -13,24 +13,26 @@ if ($age > 21){
 	echo "Younger than 20";
 }
 
+echo "\n";
+
 function greet($name){
 	echo "Hello $name";
 }
 
 greet("Eda");
-
+echo "\n";
 
 
 $cities = ["Denver", "Colorado", "Dallas"];
 echo "First city $cities[0]";
-
+echo "\n";
 $matrix = [
 	[1, 2, 3], 
 	[4, 5, 6]
 ];
 
 echo $matrix[0][0];
-
+echo "\n";
 echo strtoupper($cities[0]);
 
 echo "\n";
@@ -38,12 +40,52 @@ $text = "this is a sentence";
 $capitalized = ucfirst($text);
 echo $capitalized;
 
+echo "\n";
+
 $words = ["apple", "bannana", "pear"];
 $uppercased = array_map("strtoupper", $words);
 echo $uppercased[0];
 echo $uppercased[1];
 echo $uppercased[2];
 
+echo "\n";
+
+$mystr = "Hello world";
+$mystr = str_replace("world", "PHP", $mystr);
+echo $mystr;
+
+echo "\n";
+
+echo trim("   \n  Hello    ");
+echo "\n";
+echo date("m"); 
+echo date("d/m/Y H:i:s");
+
+echo "\n";
+echo max(1, 2, 3); // 3
+
+
+echo "\n";
+
+// Usage in forms
+$formData;
+if (isset($formData)){
+	echo "Set";
+} else {
+	echo "Not set";
+}
+
+echo "\n";
+if (empty($formData)){
+	echo "Empty";
+} else {
+	echo "Not empty";
+}
+
+echo "\n";
+echo rand(1, 100);
+echo "\n";
+echo round(3.141592, 3);
 // $name = readline("Enter your name");
 // echo "Hello $name";
 
